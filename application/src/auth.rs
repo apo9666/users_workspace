d3fd9e6d21923d8761301b72f911821e8ac3f022
apo9666 <66564120+apo9666@users.noreply.github.com>
2025-11-05 23:29:27 -0300

    Refactor authentication module to include JWT token generation and update dependencies in Cargo.toml and Cargo.lock

--- application/src/auth.rs
+++ application/src/auth.rs
@@ -1,9 +1,21 @@
-use std::sync::Arc;
-use bcrypt::{DEFAULT_COST, hash, verify};
 use crate::port::credential_repository::{Credential, CredentialRepository};
+use bcrypt::{DEFAULT_COST, hash, verify};
+use jsonwebtoken::{EncodingKey, Header, encode};
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
 
 pub struct Auth {
     credential_repository: Arc<dyn CredentialRepository>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct Claims {
+    // aud: String, // Optional. Audience
+    exp: usize, // Required (validate_exp defaults to true in validation). Expiration time (as UTC timestamp)
+    // iat: usize, // Optional. Issued at (as UTC timestamp)
+    // iss: String, // Optional. Issuer
+    // nbf: usize,  // Optional. Not Before (as UTC timestamp)
+    sub: String, // Optional. Subject (whom token refers to)
 }
 
 impl Auth {
@@ -27,16 +39,28 @@ impl Auth {
         &self,
         username: &str,
         password: &str,
-    ) -> Result<bool, Box<dyn std::error::Error>> {
+    ) -> Result<String, Box<dyn std::error::Error>> {
         if let Some(stored_password) = self
             .credential_repository
             .find_username(username.to_string())
             .await?
         {
             let valid = verify(password, &stored_password)?;
-            Ok(valid)
+            if valid {
+                let my_claims = Claims {
+                    sub: "b@b.com".to_owned(),
+                    exp: 10000000000,
+                };
+                let token = encode(
+                    &Header::default(),
+                    &my_claims,
+                    &EncodingKey::from_secret("secret".as_ref()),
+                )?;
+                return Ok(token);
+            }
+            Ok("".to_string())
         } else {
-            Ok(false)
+            Ok("".to_string())
         }
     }
 }
